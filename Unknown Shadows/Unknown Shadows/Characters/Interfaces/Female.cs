0682a070d1043e8dee2bd615bdff87b341527eb6
Lore Appelen <loreappelen@gmail.com>
2019-09-13 09:15:36 +0200

    characters

---
+++ Unknown Shadows/Unknown Shadows/Characters/Interfaces/Female.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unknown_Shadows.Characters.Interfaces
+{
+    class Female : IGender
+    {
+    }
+}
